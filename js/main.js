584a9c69d7c556b2f028e62daf785d659e2f95ec
LeoSoaresO <leonardo.rock22@hotmail.com>
2020-08-19 20:28:11 -0300

    Aplicando slides em Portfolio

---
+++ js/main.js
@@ -0,0 +1,13 @@
+if(window.SimpleSlide){
+new SimpleSlide({
+    slide: 'depoimento',
+    auto: true, 
+    time: 4000,
+    pauseOnHover: true, 
+});
+new SimpleSlide({
+    slide: 'portfolio-slide',
+    auto: true, 
+    time: 4000, 
+  });
+}
